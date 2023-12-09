24ff581ec434b124f24b6e4db0ff0f08aa5e28bb
Processing... <rbruno.p.o@gmail.com>
2023-12-09 20:01:39 +0000

    Add player hair movement.

--- src/main/java/timelessodyssey/view/elements/PlayerViewer.java
+++ src/main/java/timelessodyssey/view/elements/PlayerViewer.java
@@ -10,11 +10,11 @@ public class PlayerViewer implements ElementViewer<Player> {
     private final Sprite[] sprites;
 
     public PlayerViewer() throws IOException {
-        sprites = new Sprite[10];
-        for (int i = 0; i < 5; i++)
+        sprites = new Sprite[14];
+        for (int i = 0; i < 7; i++)
             sprites[i] = new Sprite("sprites/player/player-right-" + (i + 1) + ".png");
-        for (int i = 0; i < 5; i++)
-            sprites[i + 5] = new Sprite("sprites/player/player-left-" + (i + 1) + ".png");
+        for (int i = 0; i < 7; i++)
+            sprites[i + 7] = new Sprite("sprites/player/player-left-" + (i + 1) + ".png");
     }
 
     @Override
@@ -25,16 +25,18 @@ public class PlayerViewer implements ElementViewer<Player> {
     private Sprite getSprite(Player model, long frameCount) {
         int spriteIndex;
         if (model.isJumping())
-            spriteIndex = 3;
-        else if (model.isFalling())
-            spriteIndex = 4;
-        else if (model.getVelocity().x() != 0)
+            spriteIndex = 5;
+        else if (model.isFalling() && model.getVelocity().y() > 0.5)
+            spriteIndex = 6;
+        else if (Math.abs(model.getVelocity().x()) > 1.7)
+            spriteIndex = 3 + (int) ((frameCount / 3) % 2);
+        else if (Math.abs(model.getVelocity().x()) > 0.7)
             spriteIndex = 1 + (int) ((frameCount / 3) % 2);
         else
             spriteIndex = 0;
 
         if (!model.isFacingRight())
-            spriteIndex += 5;
+            spriteIndex += 7;
         return sprites[spriteIndex];
     }
 }
