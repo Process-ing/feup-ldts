ecb7b21b9c7481760cd10e8b3aa9aa93b9ab89f0
racoelhosilva <racoelhosilva@gmail.com>
2023-12-12 11:33:10 +0000

    Changed the star draw funcion

--- src/main/java/timelessodyssey/view/elements/StarViewer.java
+++ src/main/java/timelessodyssey/view/elements/StarViewer.java
@@ -8,25 +8,26 @@ public class StarViewer implements ElementViewer<Star>{
 
     @Override
     public void draw(Star model, GUI gui, long frameCount) {
-        for (double w = 2; w < 6; w++){
-            for (double h = 2; h < 6; h++){
+        for (double w = 3; w < 5; w++){
+            for (double h = 3; h < 5; h++){
                 gui.drawPixel(model.getPosition().x() + w, model.getPosition().y() + h, new TextColor.RGB(255, 255, 255));
             }
         }
-        gui.drawPixel(model.getPosition().x() + 3, model.getPosition().y(), new TextColor.RGB(255, 255, 255));
         gui.drawPixel(model.getPosition().x() + 3, model.getPosition().y()+1, new TextColor.RGB(255, 255, 255));
-        gui.drawPixel(model.getPosition().x() + 4, model.getPosition().y()+1, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 3, model.getPosition().y()+2, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 4, model.getPosition().y()+2, new TextColor.RGB(255, 255, 255));
 
-        gui.drawPixel(model.getPosition().x(), model.getPosition().y() + 4, new TextColor.RGB(255, 255, 255));
-        gui.drawPixel(model.getPosition().x() + 1, model.getPosition().y() + 3, new TextColor.RGB(255, 255, 255));
         gui.drawPixel(model.getPosition().x() + 1, model.getPosition().y() + 4, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 2, model.getPosition().y() + 3, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 2, model.getPosition().y() + 4, new TextColor.RGB(255, 255, 255));
 
+        gui.drawPixel(model.getPosition().x() + 5, model.getPosition().y() + 3, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 5, model.getPosition().y() + 4, new TextColor.RGB(255, 255, 255));
         gui.drawPixel(model.getPosition().x() + 6, model.getPosition().y() + 3, new TextColor.RGB(255, 255, 255));
-        gui.drawPixel(model.getPosition().x() + 7, model.getPosition().y() + 3, new TextColor.RGB(255, 255, 255));
 
-        gui.drawPixel(model.getPosition().x() + 3, model.getPosition().y() + 6, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 3, model.getPosition().y() + 5, new TextColor.RGB(255, 255, 255));
+        gui.drawPixel(model.getPosition().x() + 4, model.getPosition().y() + 5, new TextColor.RGB(255, 255, 255));
         gui.drawPixel(model.getPosition().x() + 4, model.getPosition().y() + 6, new TextColor.RGB(255, 255, 255));
-        gui.drawPixel(model.getPosition().x() + 4, model.getPosition().y() + 7, new TextColor.RGB(255, 255, 255));
 
     }
 }
