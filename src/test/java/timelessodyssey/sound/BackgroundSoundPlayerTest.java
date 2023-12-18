9cc8ea90f2a3f4568000a003bd7ac0026922a262
racoelhosilva <racoelhosilva@gmail.com>
2023-12-18 17:23:42 +0000

    Refactoring methods and tests to pass the tests

--- src/test/java/timelessodyssey/sound/BackgroundSoundPlayerTest.java
+++ src/test/java/timelessodyssey/sound/BackgroundSoundPlayerTest.java
@@ -8,8 +8,10 @@ import org.mockito.Mockito;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Objects;
@@ -22,22 +24,24 @@ import static org.mockito.Mockito.times;
 public class BackgroundSoundPlayerTest {
     @Test
     public void SoundLoaderWorking() throws Exception {
-        Clip sound = new SoundLoader().loadSound(AudioSystem.getAudioInputStream(Objects
-                        .requireNonNull(getClass().getClassLoader().getResource("sounds/demo.wav"))),
-                            AudioSystem.getClip());
-        assertNotNull(sound);
+        AudioInputStream audioInput = Mockito.mock(AudioInputStream.class);
+        Clip musicClip = Mockito.mock(Clip.class);
+
+        Clip sound = new SoundLoader().loadSound(audioInput, musicClip);
+        verify(musicClip, Mockito.times(1)).open(audioInput);
 
         BackgroundSoundPlayer backgroundSoundPlayer = new BackgroundSoundPlayer(sound);
         assertEquals(sound, backgroundSoundPlayer.getSound());
     }
 
     @Test
-    public void SoundLoaderException(){
+    public void SoundLoaderException() throws Exception {
+        AudioInputStream audioInput = Mockito.mock(AudioInputStream.class);
+        Clip musicClip = Mockito.mock(Clip.class);
+        doThrow(new FileNotFoundException()).when(musicClip).open(Mockito.any());
+
         Exception thrown = Assertions.assertThrows(Exception.class,
-                () -> {new BackgroundSoundPlayer(new SoundLoader()
-                        .loadSound(AudioSystem.getAudioInputStream(Objects
-                                .requireNonNull(getClass().getClassLoader().getResource("sounds/invalid.wav"))),
-                                null));},
+                () -> {new BackgroundSoundPlayer(new SoundLoader().loadSound(audioInput, musicClip));},
                 "BackgroundSoundPlayer was supposed to throw Exception");
 
         Assertions.assertEquals("Unable to load sound file!", thrown.getMessage());
